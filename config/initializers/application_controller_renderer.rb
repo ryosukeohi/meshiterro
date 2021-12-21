43752db800403708be044b831bb838c865100b43
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2021-12-21 23:48:45 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ec14efe5326cd63403c87a89ac7aca76c94968aeb5cd0f6ebb1696e5e4d93290804c3ac4981b33999e2714bb3a6867645836af23b4286fb9f47cf1c9bd5ef0c9'
