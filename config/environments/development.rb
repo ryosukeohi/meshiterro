bf08eb6a9eaadbe120299f170b0e03a6190de389
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2021-12-23 15:48:41 +0000

    [Finish]4-4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.web_console.whitelisted_ips = '18.179.43.208'
+
 end
